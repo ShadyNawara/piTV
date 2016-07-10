bd73bf96e99d8f9a3a24ec7355d9da43ee5cabe9
Shady Nawara <shandoosheri@gmail.com>
2016-07-10 08:46:42 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+apt-get update
 apt-get install xterm tint2 omxplayer php5-fpm php5 wget xdotool openbox unclutter xinit -y
 ginstall-ytdl
 echo "allowed_users=anybody" > '/etc/X11/Xwrapper.config'
