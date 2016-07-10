ec83de5168e1c93e208afbd0b581f71b141997dc
Shady Nawara <shandoosheri@gmail.com>
2016-07-10 11:31:22 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 apt-get update
 apt-get install xterm tint2 omxplayer php5-fpm git php5 wget xdotool openbox unclutter xinit -y
-wget https://raw.githubusercontent.com/shandoosheri/piTV/master/ginstall-ytdl
-chmod +x ginstall-ytdl
-./ginstall-ytdl
 echo "allowed_users=anybody" > '/etc/X11/Xwrapper.config'
 wget http://steinerdatenbank.de/software/kweb-1.7.2.tar.gz
 tar -xzf kweb-1.7.2.tar.gz
